84df05bb4255a0e5b4d15214189d2d7aa7e04d36
DarthCharles <carlos.darth@gmail.com>
2015-04-13 17:21:05 +0200

    New & Create test added

--- P_Nauticos/test/controllers/admin/brand_controller_test.rb
+++ P_Nauticos/test/controllers/admin/brand_controller_test.rb
@@ -3,32 +3,37 @@ require 'test_helper'
 class Admin::BrandControllerTest < ActionController::TestCase
   test "should get new" do
     get :new
-    assert_response :success
+    assert_response :success    
   end
 
   test "should get create" do
-    get :create
-    assert_response :success
+    get :new
+      assert_difference(Brand, :count) do
+      post :create, :brand => {:name => 'Nauticos', :cif => '10212', logo_url: 'holis'}
+      assert_response :redirect
+      assert_redirected_to :action => 'index'      
+    end
+        assert_equal 'La marca Nauticos fue añadida.', flash[:notice]
   end
 
-  test "should get update" do
-    get :update
-    assert_response :success
-  end
+  # test "should get update" do
+  #   get :update
+  #   assert_response :success
+  # end
 
-  test "should get destroy" do
-    get :destroy
-    assert_response :success
-  end
+  # test "should get destroy" do
+  #   get :destroy
+  #   assert_response :success
+  # end
 
-  test "should get show" do
-    get :show
-    assert_response :success
-  end
+  # test "should get show" do
+  #   get :show
+  #   assert_response :success
+  # end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-  end
+  # test "should get index" do
+  #   get :index
+  #   assert_response :success
+  # end
 
 end
