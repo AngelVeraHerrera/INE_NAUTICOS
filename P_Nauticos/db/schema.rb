89508cbb3f682bca368dec8e5b437a10fb01e168
DarthCharles <carlos.darth@gmail.com>
2015-04-20 19:05:14 +0200

    file chooser added, editing pending

--- P_Nauticos/db/schema.rb
+++ P_Nauticos/db/schema.rb
@@ -16,17 +16,23 @@ ActiveRecord::Schema.define(version: 20150416201452) do
   create_table "brands", force: true do |t|
     t.integer  "cif"
     t.string   "name"
-    t.string   "logo_url"
+    t.string   "logo_url_file_name"
+    t.string   "logo_url_content_type"
+    t.integer  "logo_url_file_size"
+    t.datetime "logo_url_updated_at"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "products", force: true do |t|
-    t.integer  "brand_id",    null: false
+    t.integer  "brand_id",                 null: false
     t.string   "name"
     t.float    "price"
     t.string   "description"
-    t.string   "product_img"
+    t.string   "product_img_file_name"
+    t.string   "product_img_content_type"
+    t.integer  "product_img_file_size"
+    t.datetime "product_img_updated_at"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
