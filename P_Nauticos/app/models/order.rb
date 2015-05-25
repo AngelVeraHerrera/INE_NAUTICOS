4c0464a2f5ac2d15b4b7207e6b1e1463f8a0e23c
DarthCharles <carlos.darth@gmail.com>
2015-05-25 10:15:38 +0200

    order and order_items model updated

--- P_Nauticos/app/models/order.rb
+++ P_Nauticos/app/models/order.rb
@@ -1,2 +1,7 @@
 class Order < ActiveRecord::Base
+  attr_accessor :card_type, :card_number, :card_expiration_month, :card_expiration_year,
+  :card_verification_value
+
+  has_many :order_items
+  has_many :books, :through => :order_items
 end
--- P_Nauticos/app/models/order_items.rb
+++ P_Nauticos/app/models/order_items.rb
@@ -1,2 +1,5 @@
 class OrderItems < ActiveRecord::Base
+
+  belongs_to :order
+  belongs_to :product
 end
